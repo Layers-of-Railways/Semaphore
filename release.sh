e9eda36538d8d8f2c29446f71a20149ea931ea6d
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-06-14 11:10:06 -0700

    fix properties parsing

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=$(grep 'mod_version = ' "./gradle.properties" | cut -d'=' -f2 | tr -d ' ')
+version=$(grep -E 'mod_version(\s*)=(\s*)' "./gradle.properties" | cut -d'=' -f2 | tr -d ' ')
 
 git tag -s -a v"$version" -m "$1"
 
